5c4d5a413be5e6a57c44946dee818d02567c28e9
Steven Turturo <steven@geekhq.us>
2016-09-26 07:30:44 -0700

    Created skeleton of non working app

--- app.py
+++ app.py
@@ -1,12 +1,58 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import (LoginManager, login_user, logout_user, login_required,
+                         current_user)
+
+import models
+import forms
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    """Given a userid, return the associated User object.
+
+    :param unicode userid: userid (email) user to retrieve
+    """
+    return User.query.get(userid)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        login_user(user)
+
+        flask.flash('Logged in successfully')
+
+        return redirect( url_for('home'))
+    else:
+        flash("Your email or password were incorrect please try again!",
+              "error")
+    return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    flash("You've been logged out! Come back soon!")
+    return redirect( url_for('index'))
+
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
 def home():
-    return 'Hello World'
-
-
+    return render_template('home.html')
--- models.py
+++ models.py
@@ -1,5 +1,38 @@
 import datetime
+import bcrypt
 
 from flask_bcrypt import generate_password_hash
 from flask_login import UserMixin
-`
+from sqlalchemy import Column, Integer, Text, TypeDecorator
+from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.orm import validates
+
+from app import db
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String())
+    email = db.Column(db.String())
+    password = db.Column(db.String())
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's Requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported at the moment."""
+        return False
+
+
+
