0e310f4b2abb7eebb4459f9a4e46a2b0ffcbeeb0
Steven Turturo <steven@geekhq.us>
2016-09-26 06:31:03 -0700

    Created config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'asjkdhfa56sd4fa6s81d3Sd1fa638sf43as8d1vc3as8d'
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
